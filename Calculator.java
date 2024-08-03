d54fbbf366a461afeae38d53e6cedb11ef7e167e
Ken <ken@example.com>
2024-08-04 00:18:30 +0800

    add_comment

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
 //test
+//test_dony
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
@@ -7,8 +8,16 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-
+    // 乘法
     public int multiply(int a, int b) {
         return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    // hello world
+    public String greeting() {
+        return "Hello Wrold";
     }
 }
