4645d2a767b55b3de0e33a082b9df3ed6742839b
ChengArcher <milkshakecrisp@gmail.com>
2024-08-04 13:31:41 +0800

    add Jenkinsfile
    add Jenkinsfile

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,13 @@
-//test
 public class Calculator {
     public int add(int a, int b) {
-        return a + b;
+        return a+b;
     }
 
     public int subtract(int a, int b) {
-        return a - b;
+        return a-b;
     }
 
     public int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public int anannihao(int a, int b) {
-        return 1+1;
+        return a*b;
     }
 }
